092227b0de6ca623d8ae5a848b27b8fa2058bb60
ItchiSushi <dpengmarais99@gmail.com>
2024-10-02 23:37:12 +0200

    - Updated the code for constant time.
    - Created the linear time program.

--- Big O Notation/Constant O(1)/constant_o(1).py
+++ Big O Notation/Constant O(1)/constant_o(1).py
@@ -1,9 +1,9 @@
 # Create a function that will parse a list to be used in the function.
 def function(list_of_numbers):
     # Create a for loop that will loop for the amount of items inside the list
-    for i in range(len(list_of_numbers)):
+    for i in list_of_numbers:
         # Create value variable and multiply the index inside the list by 2.
-        value = list_of_numbers[i] * 2
+        value = i * 2
         # Display the value.
         print(str(value))
 # Create a list to store integers.
