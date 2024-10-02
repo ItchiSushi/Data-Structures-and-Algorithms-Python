5f9f5a8e8204912979643f308bff62e33caee047
ItchiSushi <dpengmarais99@gmail.com>
2024-10-03 00:04:14 +0200

    - Completed the code for the Linear Time O(n) program
    - Added comments

---
+++ Big O Notation/Linear O(n)/linear_o(n).py
@@ -0,0 +1,10 @@
+# Create a function that will perform the linear time O(n)
+def function(list_of_numbers):
+    # Create a for loop which is better suited for linear time
+    for i in list_of_numbers:
+        # Display
+        print(i)
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14 ,15 ,16, 17, 18, 19 ,20]
+# Call the linear time function
+function(numbers)
