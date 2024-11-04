17f0af95686c0e3dd96123809bd3c1d4245e52d3
ItchiSushi <dpengmarais99@gmail.com>
2024-11-04 10:47:30 +0200

    Completed Recursion and added readme file.

--- Recursion/Big O of Recusive Functions/README.md
+++ Recursion/Big O of Recusive Functions/README.md
@@ -1,1 +1,8 @@
 <h1>Big O of Recursive Functions</h1>
+
+<h2>What to Consider in Determining the Complexity of Recursive Functions</h2>
+
+ - The number of stack frames
+ - The number of recursive calls
+ - The other operations done inside the function
+ - The space complexity (the max number of stack frames)
--- Recursion/Big O of Recusive Functions/big_o_of_recursive_functions.py
+++ Recursion/Big O of Recusive Functions/big_o_of_recursive_functions.py
@@ -1,13 +1,51 @@
+# Time: O(n)
+# Space: O(n)
 def a(n):
     if n <= 1:
         return
     a(n - 1)
 
 print(a(6))
-
+# Time: O(n/2) -> O(n)
+# Space: O(n/2) -> O(n)
 def b(n):
     if n <= 1:
         return
-    b(n -2)
+    b(n - 2)
 
-print(b(3))
+print(b(2))
+# Time: O(logn)
+# Space: O(logn)
+def c(n):
+    if n <= 1:
+        return
+    c(n / 2)
+
+print(c(6))
+# Time: O(2^n)
+# Space: O(n)
+def d(n):
+    if n <= 1:
+        return
+    d(n - 1)
+    d(n - 1)
+
+print(d(6))
+# Time: O(n^2) -> O(n)
+# Space: O(n)
+def f(n):
+    if n <= 1:
+        return
+    for _ in range(n):
+        f(n - 1)
+
+items = [1,2,3,4,5,6,7,8,9,10]
+
+print(f(7))
+# Time: O(nlogn)
+# Space: O(logn)
+def g(n):
+    if n <= 1:
+        return
+    for _ in range(n):
+        g(n / 2)
